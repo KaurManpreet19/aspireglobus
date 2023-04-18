50074f3cc9a9cd1c5d984e12bd2fda74d1219aa1
KaurManpreet19 <hansmanpreet1993@gmail.com>
2023-04-19 02:05:49 +0530

    Fonts

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -2,222 +2,224 @@ jQuery(function ($) {
     'use strict';
 
     // Menu JS
-    $(window).on('scroll', function() {
-		if ($(this).scrollTop() > 50) {
-			$('.nav').addClass('fixed');
-		} else {
-			$('.nav').removeClass('fixed');
-		}
-    });	
+    $(window).on('scroll', function () {
+        if ($(this).scrollTop() > 50) {
+            $('.nav').addClass('fixed');
+        } else {
+            $('.nav').removeClass('fixed');
+        }
+    });
     // Menu JS
-    $(window).on('scroll', function() {
-		if ($(this).scrollTop() > 50) {
-			$('.mobile-header-menu').addClass('fixed');
-		} else {
-			$('.mobile-header-menu').removeClass('fixed');
-		}
-    });	
+    $(window).on('scroll', function () {
+        if ($(this).scrollTop() > 50) {
+            $('.mobile-header-menu').addClass('fixed');
+        } else {
+            $('.mobile-header-menu').removeClass('fixed');
+        }
+    });
 
     // Mean Menu JS
-	jQuery('.mean-menu').meanmenu({
-	    meanScreenWidth: '991'
-	});
+    jQuery('.mean-menu').meanmenu({
+        meanScreenWidth: '991'
+    });
 
     // Search JS
-    $('#close-btn').on('click', function() {
+    $('#close-btn').on('click', function () {
         $('#search-overlay').fadeOut();
         $('#search-btn').show();
     });
-    $('#search-btn').on('click', function() {
+    $('#search-btn').on('click', function () {
         $('#search-overlay').fadeIn();
     });
 
     // Button Hover JS
-    $(function() {
+    $(function () {
         $('.common-btn')
-        .on('mouseenter', function(e) {
-            var parentOffset = $(this).offset(),
-            relX = e.pageX - parentOffset.left,
-            relY = e.pageY - parentOffset.top;
-            $(this).find('span').css({top:relY, left:relX})
-        })
-        .on('mouseout', function(e) {
-            var parentOffset = $(this).offset(),
-            relX = e.pageX - parentOffset.left,
-            relY = e.pageY - parentOffset.top;
-            $(this).find('span').css({top:relY, left:relX})
-        });
+            .on('mouseenter', function (e) {
+                var parentOffset = $(this).offset(),
+                    relX = e.pageX - parentOffset.left,
+                    relY = e.pageY - parentOffset.top;
+                $(this).find('span').css({ top: relY, left: relX })
+            })
+            .on('mouseout', function (e) {
+                var parentOffset = $(this).offset(),
+                    relX = e.pageX - parentOffset.left,
+                    relY = e.pageY - parentOffset.top;
+                $(this).find('span').css({ top: relY, left: relX })
+            });
     });
 
     // Wow JS
     new WOW().init();
 
     // Banner Slider JS
-	$('.banner-slider').owlCarousel({
-		items: 1,
-		loop: true,
-		margin: 0,
-		nav: false,
-		dots: true,
-		smartSpeed: 1000,
-		autoplay: true,
-		autoplayTimeout: 4000,
-		autoplayHoverPause: true,
-		animateOut: 'fadeOut',
-		animateIn: 'fadeIn',
+    $('.banner-slider').owlCarousel({
+        items: 1,
+        loop: true,
+        margin: 0,
+        nav: false,
+        dots: true,
+        smartSpeed: 1000,
+        autoplay: true,
+        autoplayTimeout: 4000,
+        autoplayHoverPause: true,
+        animateOut: 'fadeOut',
+        animateIn: 'fadeIn',
     });
-    
+
     // Banner Slider Two JS
-	$('.banner-slider-two').owlCarousel({
-		items: 1,
-		loop: true,
-		margin: 0,
-		nav: true,
-		dots: false,
-		smartSpeed: 1000,
-		autoplay: true,
-		autoplayTimeout: 4000,
-		autoplayHoverPause: true,
-		animateOut: 'fadeOut',
+    $('.banner-slider-two').owlCarousel({
+        items: 1,
+        loop: true,
+        margin: 0,
+        nav: true,
+        dots: false,
+        smartSpeed: 1000,
+        autoplay: true,
+        autoplayTimeout: 4000,
+        autoplayHoverPause: true,
+        animateOut: 'fadeOut',
         animateIn: 'fadeIn',
         navText: [
             "<i class='bx bx-chevron-left'></i>",
             "<i class='bx bx-chevron-right'></i>"
         ],
-	});
+    });
 
     // Logo Slider JS
-	$('.logo-slider').owlCarousel({
-		loop: true,
-		margin: 0,
-		nav: false,
-		dots: false,
-		smartSpeed: 1000,
-		autoplay: true,
-		autoplayTimeout: 3000,
+    $('.logo-slider').owlCarousel({
+        loop: true,
+        margin: 0,
+        nav: false,
+        dots: false,
+        smartSpeed: 1000,
+        autoplay: true,
+        autoplayTimeout: 3000,
         autoplayHoverPause: true,
-        responsive:{
-            0:{
+        responsive: {
+            0: {
                 items: 2,
             },
-            600:{
+            600: {
                 items: 3,
             },
-            1000:{
+            1200: {
                 items: 5,
             }
         }
     });
-    
+
     // Projects Slider JS
-	$('.projects-slider').owlCarousel({
-		loop: true,
-		margin: 20,
-		nav: true,
-		dots: false,
-		smartSpeed: 1000,
-		autoplay: true,
-		autoplayTimeout: 3000,
+    $('.projects-slider').owlCarousel({
+        loop: true,
+        margin: 20,
+        nav: true,
+        dots: false,
+        smartSpeed: 1000,
+        autoplay: true,
+        autoplayTimeout: 3000,
         autoplayHoverPause: true,
-        navText: [
-            "<i class='bx bx-chevron-left'></i>",
-            "<i class='bx bx-chevron-right'></i>"
-        ],
-        responsive:{
-            0:{
-                items: 1,
-            },
-            600:{
-                items: 2,
-            },
-            1000:{
-                items: 3,
-            }
-        }
-	});
+
+    });
 
     // Modal Video JS
     $('.js-modal-btn').modalVideo();
 
     // Odometer JS
-	$('.odometer').appear(function(e) {
-		var odo = $('.odometer');
-		odo.each(function() {
-			var countNumber = $(this).attr('data-count');
-			$(this).html(countNumber);
-		});
-	});
+    $('.odometer').appear(function (e) {
+        var odo = $('.odometer');
+        odo.each(function () {
+            var countNumber = $(this).attr('data-count');
+            $(this).html(countNumber);
+        });
+    });
 
     // Testimonials Slider JS
-	$('.testimonials-slider').owlCarousel({
-        items: 1,
-		loop: true,
-		margin: 0,
-		nav: true,
-		dots: false,
-		smartSpeed: 1000,
-		autoplay: true,
-		autoplayTimeout: 3000,
+    $('.testimonials-slider').owlCarousel({
+        items: 3,
+        loop: true,
+        margin: 10,
+        nav: true,
+        dots: true,
+        arrows: false,
+        smartSpeed: 1000,
+        autoplay: true,
+        autoplayTimeout: 3000,
         autoplayHoverPause: true,
         navText: [
             "<i class='bx bx-chevron-left'></i>",
             "<i class='bx bx-chevron-right'></i>"
         ],
-	});
+        responsive: {
+            0: {
+                items: 1,
+                nav: true
+            },
+            992: {
+                items: 1,
+                nav: false
+            },
+            1200: {
+                items: 3,
+                nav: true,
+                loop: false
+            }
+        }
+    });
 
     // Timer JS
-	let getDaysId = document.getElementById('days');
-	if(getDaysId !== null){
-		
-		const second = 1000;
-		const minute = second * 60;
-		const hour = minute * 60;
-		const day = hour * 24;
+    let getDaysId = document.getElementById('days');
+    if (getDaysId !== null) {
 
-		let countDown = new Date('December 25, 2022 00:00:00').getTime();
-		setInterval(function() {
-			let now = new Date().getTime();
-			let distance = countDown - now;
+        const second = 1000;
+        const minute = second * 60;
+        const hour = minute * 60;
+        const day = hour * 24;
 
-			document.getElementById('days').innerText = Math.floor(distance / (day)),
-			document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
-			document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
-			document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
-		}, second);
-	};
+        let countDown = new Date('December 25, 2022 00:00:00').getTime();
+        setInterval(function () {
+            let now = new Date().getTime();
+            let distance = countDown - now;
+
+            document.getElementById('days').innerText = Math.floor(distance / (day)),
+                document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
+                document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
+                document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
+        }, second);
+    };
 
     // Back To Top JS
-    $(function(){
-        $(window).on('scroll', function(){
+    $(function () {
+        $(window).on('scroll', function () {
             var scrolled = $(window).scrollTop();
             if (scrolled > 100) $('.go-top').addClass('active');
             if (scrolled < 100) $('.go-top').removeClass('active');
-        });  
-        $('.go-top').on('click', function() {
-            $('html, body').animate({ scrollTop: '0' },  500);
+        });
+        $('.go-top').on('click', function () {
+            $('html, body').animate({ scrollTop: '0' }, 500);
         });
     });
 
     // Accordion JS
-	$('.accordion > li:eq(0) a').addClass('active').next().slideDown();
-	$('.accordion a').on('click', function(j) {
-		var dropDown = $(this).closest('li').find('p');
-		$(this).closest('.accordion').find('p').not(dropDown).slideUp();
-		if ($(this).hasClass('active')) {
-			$(this).removeClass('active');
-		} 
-		else {
-			$(this).closest('.accordion').find('a.active').removeClass('active');
-			$(this).addClass('active');
-		}
-		dropDown.stop(false, true).slideToggle();
-		j.preventDefault();
-	});
+    $('.accordion > li:eq(0) a').addClass('active').next().slideDown();
+    $('.accordion a').on('click', function (j) {
+        var dropDown = $(this).closest('li').find('p');
+        $(this).closest('.accordion').find('p').not(dropDown).slideUp();
+        if ($(this).hasClass('active')) {
+            $(this).removeClass('active');
+        }
+        else {
+            $(this).closest('.accordion').find('a.active').removeClass('active');
+            $(this).addClass('active');
+        }
+        dropDown.stop(false, true).slideToggle();
+        j.preventDefault();
+    });
 
     // Preloader JS
-	jQuery(window).on('load', function(){
-		jQuery('.loader').fadeOut(500);
-	});
+    jQuery(window).on('load', function () {
+        jQuery('.loader').fadeOut(500);
+    });
 
     // Subscribe Form JS
     $('.newsletter-form').validator().on('submit', function (event) {
@@ -230,7 +232,7 @@ jQuery(function ($) {
             event.preventDefault();
         }
     });
-    function callbackFunction (resp) {
+    function callbackFunction(resp) {
         if (resp.result === 'success') {
             formSuccessSub();
         }
@@ -238,21 +240,21 @@ jQuery(function ($) {
             formErrorSub();
         }
     }
-    function formSuccessSub(){
+    function formSuccessSub() {
         $('.newsletter-form')[0].reset();
         submitMSGSub(true, 'Thank you for subscribing!');
-        setTimeout(function() {
+        setTimeout(function () {
             $('#validator-newsletter').addClass('hide');
         }, 4000)
     }
-    function formErrorSub(){
+    function formErrorSub() {
         $('.newsletter-form').addClass('animated shake');
-        setTimeout(function() {
+        setTimeout(function () {
             $('.newsletter-form').removeClass('animated shake');
         }, 1000)
     }
-    function submitMSGSub(valid, msg){
-        if(valid){
+    function submitMSGSub(valid, msg) {
+        if (valid) {
             var msgClasses = 'validation-success';
         } else {
             var msgClasses = 'validation-danger';
@@ -266,8 +268,8 @@ jQuery(function ($) {
         callback: callbackFunction
     });
 
-	// Switch Btn
-	$('body').append("<div class='switch-box'><label id='switch' class='switch'><input type='checkbox' onchange='toggleTheme()' id='slider'><span class='slider round'></span></label></div>");
+    // Switch Btn
+    $('body').append("<div class='switch-box'><label id='switch' class='switch'><input type='checkbox' onchange='toggleTheme()' id='slider'><span class='slider round'></span></label></div>");
 }(jQuery));
 
 
@@ -293,7 +295,7 @@ function toggleTheme() {
         document.getElementById('slider').checked = false;
     } else {
         setTheme('theme-light');
-      document.getElementById('slider').checked = true;
+        document.getElementById('slider').checked = true;
     }
 })();
 
